e167a3d47d85c470dae58b24f679c10da9705b36
Eduardo <etorres_57@yahoo.com>
2017-07-05 17:20:35 -0400

    supressed color from logs (#11)

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -8,7 +8,7 @@ chai.use(spies);
 
 const bunyan       = require('bunyan');
 const PrettyStream = require('bunyan-prettystream');
-const prettyStdOut = new PrettyStream({mode: 'dev'});
+const prettyStdOut = new PrettyStream({mode: 'dev', useColor: false});
 prettyStdOut.pipe(process.stdout);
 
 
